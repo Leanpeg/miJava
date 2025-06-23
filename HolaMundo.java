3cebe69753521eaa2d8073f485873c7c72e90689
Leanpeg <leninpzg@gmail.com>
2025-06-23 00:34:33 -0600

    cambio saludos

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,12 +1,13 @@
  public class HolaMundo {
- public static void main(String[] args) {
- String nombre = "Estudiante";
- int a = 10;
- int b = 7;
- int suma = a + b;
- int multiplicacion = a * b;
- System.out.println("Hola, " + nombre + "!");
- System.out.println("La suma de " + a + " + " + b + " es: " + suma);
- System.out.println("La multiplicación de " + a + " * " + b + " es: " + multiplicacion);
+  public static void main(String[] args) {
+     System.out.println("Leanpeg - Carnet 123456");
+     String nombre = "Estudiante";
+     int a = 10;
+     int b = 7;
+     int suma = a + b;
+     int multiplicacion = a * b;
+     System.out.println("Hola, " + nombre + "!");
+   System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+   System.out.println("La multiplicación de " + a + " * " + b + " es: " + multiplicacion);
  }
  }
